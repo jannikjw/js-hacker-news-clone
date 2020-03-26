451abc3da6072d483859b38adf029eb2b409ef8f
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 18:16:41 +0100

    Add constants and reducer to redux store to manage verify-account state

--- centerlingnews/src/store/reducers/index.js
+++ centerlingnews/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { register } from './register.reducer';
+import { verify } from './verify.reducer';
 
 const rootReducer = combineReducers({
   register,
+  verify
 });
 
 export default rootReducer;
