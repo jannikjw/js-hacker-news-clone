9a49ef452aa91702bd977e1bd646768feb05f3e2
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 15:14:32 +0100

    Connect mongoose to configured URL on server start

--- api/app.js
+++ api/app.js
@@ -4,10 +4,34 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 
+// load environment variables from .env file
+require("dotenv").config();
+
 const indexRouter = require("./routes/index");
 
 const apiResponse = require("./helpers/apiResponse");
 
+// setup the connection to MongoDB
+const MONGODB_URL = process.env.MONGODB_URL;
+const mongoose = require("mongoose");
+mongoose.connect(MONGODB_URL, { 
+		useNewUrlParser: true, 
+		useCreateIndex: true, 
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
+	.then(() => {
+		// don't show the log when it is test
+		if(process.env.NODE_ENV !== "test") {
+			console.log("Connected to %s", MONGODB_URL);
+			console.log("App is running ... \n");
+			console.log("Press CTRL + C to stop the process. \n");
+		}
+	})
+	.catch(err => {
+		console.error("App starting error:", err.message);
+		process.exit(1);
+	});
 
 const app = express();
 
