4bcdb0f6170ebf6e258e843ed5b6401e3c25cb17
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 21:34:41 +0100

    Add updatepassword reducer

---
+++ centerlingnews/src/store/reducers/updatepassword.reducer.js
@@ -0,0 +1,25 @@
+import { authConstants } from '../constants';
+
+const initialState = { updating: false, errors: {}, message: null };
+
+export function updatepassword(state = initialState, action) {
+  switch (action.type) {
+    case authConstants.UPDATE_PW_REQUEST_INITIATED:
+      return {
+        updating: true,
+        message: null
+      };
+    case authConstants.UPDATE_PW_REQUEST_SUCCEEDED:
+      return {
+        updating: false,
+        message: action.message
+      };
+    case authConstants.UPDATE_PW_REQUEST_FAILED:
+      return {
+        updating: false,
+        errors: action.error
+      };
+    default:
+      return state
+  }
+}
