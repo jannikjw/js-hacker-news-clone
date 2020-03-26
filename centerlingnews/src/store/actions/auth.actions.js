424438de9eb247b77550ba671596998d045f6d35
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 18:19:46 +0100

    Add verifyAccount action

--- centerlingnews/src/store/actions/auth.actions.js
+++ centerlingnews/src/store/actions/auth.actions.js
@@ -4,6 +4,7 @@ import { history } from '../../helpers';
 
 export const authActions = {
     register,
+    verifyAccount,
 };
 
 function register(firstName, lastName, username, email, password) {
@@ -25,4 +26,26 @@ function register(firstName, lastName, username, email, password) {
     function request(user) { return { type: authConstants.REGISTER_REQUEST_INITIATED, user } }
     function success(user) { return { type: authConstants.REGISTER_REQUEST_SUCCEEDED, user } }
     function failure(error) { return { type: authConstants.REGISTER_REQUEST_FAILED, error } }
+}
+
+function verifyAccount(email, otp) {
+    return dispatch => {
+        dispatch(request({ email }));
+
+        authService.verifyAccount(email, otp)
+            .then(
+                response => { 
+                    const user = response.data
+                    dispatch(success(user));
+                    history.push('/');
+                },
+                error => {
+                    dispatch(failure(error));
+                }
+            );
+    };
+
+    function request(user) { return { type: authConstants.VERIFY_REQUEST_INITIATED, user } }
+    function success(user) { return { type: authConstants.VERIFY_REQUEST_SUCCEEDED, user } } 
+    function failure(error) { return { type: authConstants.VERIFY_REQUEST_FAILED, error } }
 }
