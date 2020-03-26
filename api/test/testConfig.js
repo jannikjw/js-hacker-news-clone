b6b4e11657f5f747bb7dd43ea53e2fc4c0c17b7e
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 21:24:12 +0100

    Update testConfig to user different collection

--- api/test/testConfig.js
+++ api/test/testConfig.js
@@ -1,5 +1,6 @@
 //During the automated test the env variable, We will set it to 'test'
 process.env.NODE_ENV = 'test';
+process.env.MONGODB_URL = 'mongodb://127.0.0.1:27017/centerling-news-test';
 
 //Require the dev-dependencies
 let chai = require('chai');
