0ce01bba6fea18c18d893f3f933439f4761f6368
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 15:22:39 +0100

    Add AuthController with dummy route

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,16 @@
+
+/**
+ * User registration.
+ *
+ * @param {string}      firstName
+ * @param {string}      lastName
+ * @param {string}      username
+ * @param {string}      email
+ * @param {string}      password
+ *
+ * @returns {Object}
+ */
+exports.register = [
+    (req, res) => {
+        res.json('hello world');
+	}];
