32bd9d395eca663e8f63c93c186a179c9af9e673
Michael Froehlich <m.froehlich@tum.de>
2020-03-26 22:06:34 +0100

    Add reducer and constants to track state of request a password reset link flow in redux store

---
+++ centerlingnews/src/store/reducers/forgot.reducer.js
@@ -0,0 +1,28 @@
+import { authConstants } from '../constants';
+
+const initialState = { sending: false, errors: {}, message: null };
+
+export function forgot(state = initialState, action) {
+  switch (action.type) {
+    case authConstants.FORGOT_PW_REQUEST_INITIATED:
+      return {
+        sending: true,
+        message: null,
+        errors: state.errors
+      };
+    case authConstants.FORGOT_PW_REQUEST_SUCCEEDED:
+      return {
+        sending: false,
+        message: action.message,
+        errors: null
+      };
+    case authConstants.FORGOT_PW_REQUEST_FAILED:
+      return {
+        sending: false,
+        errors: action.error,
+        message: null
+      };
+    default:
+      return state
+  }
+}
--- centerlingnews/src/store/reducers/index.js
+++ centerlingnews/src/store/reducers/index.js
@@ -5,13 +5,15 @@ import { verify } from './verify.reducer';
 import { requestNewVerificationCode } from './requestNewVerificationCode.reducer';
 import { login } from './login.reducer';
 import { updatepassword } from './updatepassword.reducer';
+import { forgot } from './forgot.reducer';
 
 const rootReducer = combineReducers({
   register,
   verify,
   requestNewVerificationCode,
   login,
-  updatepassword
+  updatepassword,
+  forgot
 });
 
 export default rootReducer;
