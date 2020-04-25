0a41d9ffbb440ac5f17cda43cc49fb2de89b7473
Lunter <jayjay1110@hotmail.de>
2020-04-25 18:25:02 +0200

    Frontend: - added Navbar to all pages

---
+++ centerlingnews/src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import "./Navbar.scss";
+
+
+export default class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar">
+        <div className="collpase navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Centerling News</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/posts" className="nav-link">Posts</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/submit" className="nav-link">Create Post</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/profile" className="nav-link">Profile</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/login" className="nav-link">Login</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/register" className="nav-link">Sign Up</Link>
+            </li>
+
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
